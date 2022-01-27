00a4a2e2814e2da28f224e25e381487a3672478d
Josh <dev@cldv.org>
2022-01-28 00:05:24 +0100

    update demo

--- example/script.js
+++ example/script.js
@@ -21,41 +21,66 @@ function hsv2rgb(hue, saturation, value) {
     let m = value - chroma
     let [r,g,b] = [r1+m, g1+m, b1+m]
 
-    return [255*r,255*g,255*b]
+    return [parseInt(255*r),parseInt(255*g),parseInt(255*b)]
 }
 
 var ghue = 0
 var ghue_steps = 20
+var gvalue_steps = .2
+var gvalue_start = .2
 var gcolorarray = []
-var gcolor_index = 0
+var gcolor_index = Math.round(Math.random() * 7)
+var gcolor_palette = 4
+var gcolor_max_palettes = 5
+var eraser = false
+var brushSize = document.querySelector('#medium').dataset.size || 2
 
+let i = 0
+// shades of gray
+gcolorarray[i] = []
+let value = 0;
 for (ghue = 0; ghue <= 360; ghue += ghue_steps) {
-    gcolorarray.push(hsv2rgb(ghue, .80, 1))
+    gcolorarray[i].push(hsv2rgb(360, 0, value))
+    value += (100 / (360 / ghue_steps)) / 100
 }
-gcolorarray.push([0,0,0])
-gcolorarray.push([255,255,255])
+++i
 
-function circleColor(color, increase = true) {
-    if (increase == false) {
-        if (1+gcolor_index >= gcolorarray.length) return gcolorarray[0]
-        return gcolorarray[1+gcolor_index]
+// colored
+for (let gvalue = gvalue_start; gvalue <= 1; gvalue += gvalue_steps) {
+    gcolorarray[i++] = []
+    for (ghue = 0; ghue <= 360; ghue += ghue_steps) {
+        gcolorarray[i - 1].push(hsv2rgb(ghue, 1, gvalue))
     }
-    if (++gcolor_index >= gcolorarray.length) gcolor_index = 0
-        return gcolorarray[gcolor_index]
 }
 
+gcolor_max_palettes = gcolorarray.length - 1
+
+
+
+function circleColor(increase = true) {
+    if (increase == false) {
+        if (1+gcolor_index >= gcolorarray[gcolor_palette].length) return gcolorarray[gcolor_palette][1]
+        return gcolorarray[gcolor_palette][1+gcolor_index]
+    }
+    if (++gcolor_index >= gcolorarray[gcolor_palette].length) gcolor_index = 1
+        return gcolorarray[gcolor_palette][gcolor_index]
+}
 
 // @source: https://stackoverflow.com/a/67723999/10495683 (modified)
-let elementCurrentColor = document.querySelector('#currentColor');
+let elementCurrentColor = document.querySelector('#currentColor')
+let next = gcolorarray[gcolor_palette].length > gcolor_index + 1 ? gcolor_index + 1 : 0
+elementCurrentColor.style.backgroundColor = 'rgb(' + gcolorarray[gcolor_palette][next].join(', ') + ')'
+
 var canvas, ctx, flag = false,
     prevX = 0,
     currX = 0,
     prevY = 0,
     currY = 0,
     dot_flag = false
+var colorLocked = false    
 
 var x = 'rgb(' + gcolorarray[0].join(', ') + ')',
-    y = 3
+    y = 1.5
     
 function init() {
     canvas = document.getElementById('canvas')
@@ -63,7 +88,12 @@ function init() {
     w = canvas.width
     h = canvas.height
 
-    canvas.addEventListener("mousemove", function (e) {
+    let tmpColor = ctx.fillStyle
+    ctx.fillStyle = '#111'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = tmpColor
+
+    document.addEventListener("mousemove", function (e) {
         findxy('move', e)
     }, false)
     canvas.addEventListener("mousedown", function (e) {
@@ -72,7 +102,7 @@ function init() {
     canvas.addEventListener("mouseup", function (e) {
         findxy('up', e)
     }, false)
-    canvas.addEventListener("mouseout", function (e) {
+    document.addEventListener("mouseup", function (e) {
         findxy('out', e)
     }, false)
     canvas.addEventListener("touchmove", function (e) {
@@ -87,15 +117,29 @@ function init() {
     canvas.addEventListener("touchcancel", function (e) {
         findxy('out', e)
     }, false)
+
+    document.querySelector('.loading').classList.remove('loading')
 }
  
 function draw() {
     ctx.beginPath()
     ctx.moveTo(prevX, prevY)
+    let tmpColor = ctx.fillStyle
+    if (eraser) {
+        // @TODO replace with proper erase when the undo is implemented properly
+        ctx.fillStyle = '#111'
+        ctx.strokeStyle = '#111'
+    }
+    ctx.lineWidth = y * brushSize
     ctx.lineTo(currX, currY)
-    ctx.lineWidth = y
+    ctx.lineJoin = ctx.lineCap = 'round'
     ctx.stroke()
     ctx.closePath()
+    cPush()
+    if (eraser) {
+        ctx.fillStyle = tmpColor
+        ctx.strokeStyle = tmpColor
+    }
 }
 
 function findxy(res, e) {
@@ -110,15 +154,17 @@ function findxy(res, e) {
 
         flag = true
         dot_flag = true
-        if (dot_flag) {
+        if (dot_flag && !eraser) {
             ctx.beginPath()
-            let color = ctx.getImageData(currX, currY, 1, 1).data
-            let tmp = circleColor(color)
-            let tmpColor = 'rgb(' + tmp.join(', ') + ')'
-            elementCurrentColor.style.backgroundColor = 'rgb(' + circleColor(tmp, false).join(', ') + ')'
-            ctx.fillStyle = tmpColor
-            ctx.strokeStyle = tmpColor
-            ctx.fillRect(currX, currY, 2, 2)
+            if (!colorLocked) {
+                let tmp = circleColor()
+                let tmpColor = 'rgb(' + tmp.join(', ') + ')'
+                elementCurrentColor.style.backgroundColor = 'rgb(' + circleColor(false).join(', ') + ')'
+                ctx.fillStyle = tmpColor
+                ctx.strokeStyle = tmpColor
+            }
+            ctx.arc(currX, currY, (y * brushSize) / 2, 0, 2 * Math.PI, false)
+            ctx.fill();
             ctx.closePath()
             dot_flag = false
         }
@@ -134,16 +180,81 @@ function findxy(res, e) {
             newY = e.clientY || e.touches[0].clientY
             currX = newX - canvas.getBoundingClientRect().left
             currY = newY - canvas.getBoundingClientRect().top
+            if (0 > prevX || 0 > prevY ||
+                w < prevX || h < prevY) {
+                prevX = currX
+                prevY = currY
+            }
             draw()
         }
     }
 }
+
+function changeColor (e = null, setElement = true) {
+    if (e) {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+    let tmp = circleColor()
+    let tmpColor = 'rgb(' + tmp.join(', ') + ')'
+    if (setElement) {
+        elementCurrentColor.style.backgroundColor = 'rgb(' + circleColor(false).join(', ') + ')'
+    }
+    ctx.fillStyle = tmpColor
+    ctx.strokeStyle = tmpColor
+}
+
+function toggleColorPalette (e) {
+    e.stopPropagation()
+    let index = e.currentTarget.dataset.number
+    index = (index >= 0 && index < gcolor_max_palettes) ? ++index : 0
+
+    gcolor_palette = index
+    e.currentTarget.dataset.number = index
+    e.currentTarget.innerHTML = index + 1 // just for visuals ;)
+
+    let tmp = gcolorarray[gcolor_palette][gcolor_index]
+    let tmpColor = 'rgb(' + tmp.join(', ') + ')'
+    elementCurrentColor.style.backgroundColor = 'rgb(' + circleColor(false).join(', ') + ')'
+    ctx.fillStyle = tmpColor
+    ctx.strokeStyle = tmpColor
+}
+
+// undo script
+// @source: https://www.codicode.com/art/undo_and_redo_to_the_html5_canvas.aspx
+// note: this is not an optimal approach. as @Max said in the comments of this article:
+// "But saving the whole canvas as ana image for undo or redo, is memory intensive, and a performance killer."
+// A better aproach would be this: https://stackoverflow.com/questions/17150610/undo-redo-for-paint-program-using-canvas
+// @TODO future me, because I am too tired right now
+var cPushArray = new Array();
+var cStep = -1;
+	
+function cPush () {
+    cStep++;
+    if (cStep < cPushArray.length) { cPushArray.length = cStep; }
+    cPushArray.push(canvas.toDataURL());
+}
+
 
 // controls:
 let toggleButton = document.querySelector('#toggleCanvas')
 let clearButton = document.querySelector('#clearButton')
 let saveButton = document.querySelector('#saveButton')
 let reportButton = document.querySelector('#flagButton')
+let closeButton = document.querySelector('#closeButton')
+let expandButton = document.querySelector('#expandButton')
+let additionalControls = document.querySelector('#additionalControls')
+let undoButton = document.querySelector('#undoButton')
+let eraserEl = document.querySelector('#eraser')
+let sizeSmall = document.querySelector('#small')
+let sizeMedium = document.querySelector('#medium')
+let sizeLarge = document.querySelector('#large')
+let sizeExtraLarge = document.querySelector('#extralarge')
+let toggleColorPaletteButton = document.querySelector('#colorPalette')
+let toggleColor = document.querySelector('#currentColor')
+let lockColor = document.querySelector('.colorContainer')
+toggleColor.addEventListener('touchend', changeColor)
+toggleColor.addEventListener('click', changeColor)
 
 function toggleCanvas (e) {
     if (toggleButton.classList.contains('show')) {
@@ -168,6 +279,15 @@ window.onscroll = function() {
     }
 }
 
+function expandAdditionalControls () {
+    additionalControls.classList.toggle('show')
+    if (additionalControls.classList.contains('show')) {
+        expandButton.innerHTML = expandButton.dataset.hide
+    } else {
+        expandButton.innerHTML = expandButton.dataset.show
+    }
+}
+
 function showAlert(text, prefix = '') {
     let alerts = document.querySelector('.alerts ul')
     let ealert = document.createElement('li')
@@ -188,8 +308,12 @@ function showAlert(text, prefix = '') {
 }
 
 
-function clearCanvas () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+function clearCanvas (e) {
+    e.preventDefault()
+    let tmpColor = ctx.fillStyle
+    ctx.fillStyle = '#111'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = tmpColor
     showAlert('canvas cleared')
 }
 
@@ -200,12 +324,13 @@ function isCanvasBlank(canvas) {
         context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
     )
 
-    return !pixelBuffer.some(color => color !== 0)
+    return !pixelBuffer.some(color => color !== 0 && color !== 4279308561)
 }
 
 function closeLightbox(el) {
     document.querySelector('body').classList.remove('show-lightbox')
     document.querySelector('#lightbox').classList.remove('verified')
+    document.querySelector('#lightbox').classList.remove('no-report')
     document.querySelector('#lightbox .image-wrapper').innerHTML = ''
 }
 
@@ -216,30 +341,89 @@ function openLightbox(image) {
     verified = image.dataset.verified
     if (verified == 'True') {
         document.querySelector('#lightbox').classList.add('verified')
+    } else if (verified == undefined) {
+        document.querySelector('#lightbox').classList.add('no-report')
     }
     img = document.createElement('img')
     img.src = image.src
     document.querySelector('#lightbox .image-wrapper').appendChild(img)
 }
 
-function saveCanvas () {
+let canSave = true
+let canSaveTimerRunning = false
+function resetCanSave () {
+    if (!canSaveTimerRunning) {
+        canSaveTimerRunning = true
+        setTimeout(() => {
+            canSave = true
+            canSaveTimerRunning = false
+        }, 5000)
+    }
+}
+
+function saveCanvas (e) {
+    e.preventDefault()
     if (isCanvasBlank(canvas)) {
         showAlert('canvas is empty', 'error')
         return
     }
+    
+    if (!canSave) {
+        showAlert('too many requests', 'error')
+        return
+    } else {
+        canSave = false
+    }
+
 
     canvas.toBlob(function(blob) {
         const formData = new FormData()
         formData.append('file', blob, 'filename')
-    
-        showAlert('[demo] image saved', 'success')
+        
+        document.querySelector('body').classList.add('loading')
+        
+        // success
+        showAlert('image saved [demo]', 'success')
         var image = document.createElement('img')
         image.src = URL.createObjectURL(blob)
-        document.querySelector('#gallery').appendChild(image)
+        document.querySelector('#gallery').prepend(image)
 
         image.addEventListener('click', openLightbox)
         image.addEventListener('touch', openLightbox)
+
+        document.querySelector('body').classList.remove('loading')
+        resetCanSave()
     })
+}
+
+function undo () {
+    if (cStep > 0) {
+        cStep--
+        var canvasPic = new Image()
+        canvasPic.src = cPushArray[cStep]
+        canvasPic.onload = function () { 
+            let tmpColor = ctx.fillStyle
+            ctx.fillStyle = '#111'
+            ctx.fillRect(0, 0, canvas.width, canvas.height)
+            ctx.fillStyle = tmpColor
+            ctx.drawImage(canvasPic, 0, 0)
+        }
+    }
+}
+
+function toggleEraser () {
+    if (eraserEl.checked) {
+        eraser = true
+    } else {
+        eraser = false
+    }
+}
+
+function toggleBrushSize (e) {
+    el = e.currentTarget
+    if (el.checked) {
+        brushSize = el.dataset.size
+    }
 }
 
 function report () {
@@ -248,15 +432,38 @@ function report () {
     const formData = new FormData()
     formData.append('image', filename)
 
-    showAlert('[demo] report failed', 'error')
+    document.querySelector('body').classList.add('loading')
+    
     closeLightbox()
+    document.querySelector('body').classList.remove('loading')
+    showAlert('cannot report in [demo]', 'error')
+    closeLightbox()
+    document.querySelector('body').classList.remove('loading')
 }
 
 ['click', 'touch'].forEach(function(e) {
     toggleButton.addEventListener(e, toggleCanvas)
     clearButton.addEventListener(e, clearCanvas)
     saveButton.addEventListener(e, saveCanvas)
+    eraserEl.addEventListener(e, toggleEraser)
+    sizeSmall.addEventListener(e, toggleBrushSize)
+    sizeMedium.addEventListener(e, toggleBrushSize)
+    sizeLarge.addEventListener(e, toggleBrushSize)
+    sizeExtraLarge.addEventListener(e, toggleBrushSize)
+    expandButton.addEventListener(e, expandAdditionalControls)
+    toggleColorPaletteButton.addEventListener(e, toggleColorPalette)
+    lockColor.addEventListener(e, () => {
+        colorLocked = !colorLocked
+        lockColor.classList.toggle('lock')
+        
+        if (colorLocked) {
+            changeColor(null, false)
+        } else {
+            elementCurrentColor.style.backgroundColor = 'rgb(' + circleColor(false).join(', ') + ')'
+        }
+    })
     
+    closeButton.addEventListener(e, closeLightbox)
     document.querySelector('#lightbox').addEventListener(e, closeLightbox)
     document.querySelector('#lightbox .lightbox-wrapper').addEventListener(e, function(event){event.stopPropagation()}, false)
     document.querySelectorAll('#gallery img').forEach((el) => {
@@ -265,3 +472,28 @@ function report () {
 
     reportButton.addEventListener(e, report)
 })
+
+// prevent flashing on load, so start with display: none
+additionalControls.style.display = 'flex';
+
+var interval
+
+function destroyInterval (e) {
+    e.preventDefault()
+    clearInterval(interval)
+}
+
+function createInterval (e) {
+    e.preventDefault()
+    interval = setInterval(() => {
+        undo()
+    }, 50)
+}
+
+undoButton.addEventListener('mousedown', createInterval)
+undoButton.addEventListener('touchstart', createInterval)
+
+undoButton.addEventListener('mouseup', destroyInterval)
+undoButton.addEventListener('mouseout', destroyInterval)
+undoButton.addEventListener('touchend', destroyInterval)
+undoButton.addEventListener('touchcancel', destroyInterval)
